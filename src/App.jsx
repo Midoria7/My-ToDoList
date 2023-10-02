93da020a2c03df78cf0c2c583a8e9eb59f5a3464
Midoria7 <buptxmh@bupt.edu.cn>
2023-10-02 22:55:48 +0800

    Feature: Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,54 @@ import Listbar from './components/ListBar';
 import TaskBox from './components/TaskBox';
 
 export default function App() {
-	const [lists, setLists] = useState([
-		{
-			title: "Default List",
-			["To Do"]: [
+	const [lists, setLists] = useState(
+		() => {
+			return localStorage.getItem('lists')
+			? JSON.parse(localStorage.getItem('lists'))
+			: [
 				{
-					name: "Task 1",
-					id: uuid(),
-					descrip: "Description 1",
-				},
-			],
-			["Doing"]: [],
-			["Done"]: [],
-		}, 
-	]);
+					title: "Default List",
+					["To Do"]: [
+						{
+							name: "Task 1",
+							id: uuid(),
+							descrip: "Description 1",
+						},
+					],
+					["Doing"]: [],
+					["Done"]: [],
+				}, 
+			];
+		}
+	);
 	const [currentList, setCurrentList] = useState(lists[0]);
+	
+	function localStore() {
+		if (!lists.length) {
+			localStorage.setItem('lists', JSON.stringify([
+				{
+					title: "Default List",
+					["To Do"]: [
+						{
+							name: "Task 1",
+							id: uuid(),
+							descrip: "Description 1",
+						},
+					],
+					["Doing"]: [],
+					["Done"]: [],
+				}, 
+			]));
+			setLists(JSON.parse(localStorage.getItem('lists')));
+		} else {
+			localStorage.setItem('lists', JSON.stringify(lists));
+		}
+	}
+
+	useEffect(() => {
+		localStore();
+	}, [lists]);
+	
 	return (
 		<div className="App">
 			<Listbar 
